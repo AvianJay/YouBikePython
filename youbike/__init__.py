3a2fd3241768b9caa2d2deb54cc0fa6cded6a5fa
AvianJay <48914361+AvianJay@users.noreply.github.com>
2024-10-29 23:45:12 +0800

    Test

--- youbike/__init__.py
+++ youbike/__init__.py
@@ -1,5 +1,7 @@
+import sys
 import json
 import requests
+import argparse
 
 def getdata():
     headers = {
@@ -11,7 +13,9 @@ def getdata():
     response = requests.get('https://apis.youbike.com.tw/json/station-yb2.json', headers=headers)
     return response.json()
 
-def searchstation(name, data=getdata()):
+def searchstation(name, data=None):
+    if not data:
+        data = getdata()
     results = []
     for station in data:
         if name in station["name_tw"]:
@@ -22,5 +26,5 @@ def searchstation(name, data=getdata()):
             results.append(station)
     return results
 
-def main(argv):
+def main(argv=sys.argv):
     print(argv)
