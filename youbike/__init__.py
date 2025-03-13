d2b577a4cbee525fb1ee70d09eec8fc5a259d7c7
AvianJay <48914361+AvianJay@users.noreply.github.com>
2025-03-13 17:45:24 +0800

    Fix lint
    PEP 8:
     E501
     E505

--- youbike/__init__.py
+++ youbike/__init__.py
@@ -11,7 +11,12 @@ def measure(lat1, lon1, lat2, lon2):
     R = 6378.137  # Radius of earth in KM
     dLat = math.radians(lat2 - lat1)
     dLon = math.radians(lon2 - lon1)
-    a = math.sin(dLat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dLon / 2) ** 2
+    a = (
+        math.sin(dLat / 2) ** 2
+        + math.cos(math.radians(lat1))
+        * math.cos(math.radians(lat2))
+        * math.sin(dLon / 2) ** 2
+    )
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = R * c
     return d * 1000  # meters
@@ -64,7 +69,7 @@ def getstationbylocation(lat, lon, distance, data=None):
     results = []
     for station in data:
         td = measure(lat, lon, float(station["lat"]), float(station["lng"]))
-        if td<=distance:
+        if td <= distance:
             results.append(station)
     return results
 
