7f5fab2adadd214ab390d8db931b3921e8dfda62
AvianJay <48914361+AvianJay@users.noreply.github.com>
2025-03-13 17:48:23 +0800

    fix lint
    E501

--- tests/test_youbike.py
+++ tests/test_youbike.py
@@ -7,7 +7,11 @@ class TestYouBike(unittest.TestCase):
 
     def test_youbike(self):
         data = youbike.getallstations()
-        self.assertIsInstance(data, list, "getallstations() should return a list")
+        self.assertIsInstance(
+            data,
+            list,
+            "getallstations() should return a list"
+        )
 
 
 if __name__ == '__main__':
